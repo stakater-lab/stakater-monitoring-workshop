949bc785327c85e4ff02d0adbfb69a263f812b4e
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2019-12-07 16:42:03 +0500

    add destroy steps

--- application/destroy-apps-monitoring.sh
+++ application/destroy-apps-monitoring.sh
@@ -1,2 +1,11 @@
-# Destroy Application monitoring by deleting namespace
+# Destroy Nordmart Services
+#oc delete -f nordmart/.
+
+## Destroy Prometheus
+oc delete -f infra/prometheus/.
+
+## Destroy Prometheus Operator
+oc delete -f infra/operator/.
+
+# Destroy namespace
 oc delete -f namespace.yaml
