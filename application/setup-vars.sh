91de2259f98a532c22f1c83ecc4bb7b49f66b1bf
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2019-12-09 12:10:43 +0500

    add sed command for grafana

--- application/setup-vars.sh
+++ application/setup-vars.sh
@@ -7,5 +7,6 @@ sed -i -e "s/NAMESPACE/${NAMESPACE}/g" namespace.yaml
 
 sed -i -e "s/NAMESPACE/${NAMESPACE}/g" infra/operator/*
 sed -i -e "s/NAMESPACE/${NAMESPACE}/g" infra/prometheus/*
+sed -i -e "s/NAMESPACE/${NAMESPACE}/g" infra/grafana/*
 
 sed -i -e "s/NAMESPACE/${NAMESPACE}/g" nordmart/*
